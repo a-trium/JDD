25a58df55f88bc47e0de50011eb3e9f7a46e9aef
1002473 <1002473@SKP1002473MN001.local>
2015-08-25 12:20:38 +0900

    add zuckay dir

---
+++ java-8-in-action/zuckay/src/test/java/io/github/zuckay/ZuckayApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.zuckay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ZuckayApplication.class)
+public class ZuckayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ java-8-in-action/zuckay/src/test/java/io/github/zuckay/chapter4/StreamSpec.java
@@ -0,0 +1,16 @@
+package io.github.zuckay.chapter4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class StreamSpec {
+
+    @Test
+    public void test_stream01() {
+        assertEquals(1, 1);
+    }
+}
